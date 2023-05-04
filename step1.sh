25054cbd3eec9a6cd20f46e20d014e49e0bf5c25
Evgeny Chernyshov <evgeny.chernyshov@hkeeper.global>
2023-05-04 17:16:28 +0200

    initial commit

---
+++ step1.sh
@@ -0,0 +1,13 @@
+poetry run \
+	parser-2gis -i \
+	"https://2gis.ru/tomsk/search/Стоматологические центры" \
+	"https://2gis.ru/tomsk/search/Ногтевые студии" \
+	"https://2gis.ru/tomsk/search/Косметолог" \
+	"https://2gis.ru/tomsk/search/Эпиляция" \
+	"https://2gis.ru/tomsk/search/Многопрофильные медицинские центры" \
+	"https://2gis.ru/tomsk/search/Медицинские анализы" \
+	"https://2gis.ru/tomsk/search/Услуги детских врачей" \
+	"https://2gis.ru/tomsk/search/Парикмахерские" \
+	-o "C:\results.csv" -f csv
+
+
---
+++ step2.sh
@@ -0,0 +1,22 @@
+poetry run \
+        parser-2gis -i \
+	"https://2gis.ru/tomsk/search/IP-телефония" \
+	"https://2gis.ru/tomsk/search/Автоматизация бизнес-процессов" \
+	"https://2gis.ru/tomsk/search/Автоматизация медицинских учреждений" \
+	"https://2gis.ru/tomsk/search/Автоматизация предприятий общественного питания" \
+	"https://2gis.ru/tomsk/search/Автоматизация торговли" \
+	"https://2gis.ru/tomsk/search/Бухгалтерские программы" \
+	"https://2gis.ru/tomsk/search/​Дизайн рекламы" \
+	"https://2gis.ru/tomsk/search/​Инвестиционные компании" \
+	"https://2gis.ru/tomsk/search/Информационная безопасность" \
+	"https://2gis.ru/tomsk/search/Маркетинговые / социологические исследования" \
+	"https://2gis.ru/tomsk/search/Мобильные приложения" \
+	"https://2gis.ru/tomsk/search/Продажа программного обеспечения" \
+	"https://2gis.ru/tomsk/search/Размещение рекламы в интернете" \
+	"https://2gis.ru/tomsk/search/Разработка компьютерных игр" \
+	"https://2gis.ru/tomsk/search/​Разработка / поддержка / продвижение web-сайтов" \
+	"https://2gis.ru/tomsk/search/Разработка программного обеспечения" \
+	"https://2gis.ru/tomsk/search/Разработка программного обеспечения для мобильных устройств" \
+	"https://2gis.ru/tomsk/search/​Услуги системного администрирования" \
+	-o "C:\results.csv" -f csv
+
